6a144851f31c0ff57a2597eac1875a91141ad559
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-09-30 21:19:06 +0530

    updated

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,30 +1,27 @@
-import axios from "axios"
-import { useContext} from "react";
+import axios from "axios";
+import { useContext } from "react";
 import { AuthContext } from "../context/Authcontext";
 
-
 export const Login = () => {
+  const { setUser } = useContext(AuthContext);
 
-    const {setUser}= useContext(AuthContext)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = new FormData(e.target);
-        const email = data.get("email");
-        const password = data.get("password");
-        try {
-            const response = await axios.post("http://localhost:8080/api/auth/login", { email, password }, { withCredentials: true });
-            if (response.status == 200) {
-              setUser(response.data);
-              // console.log(response.data);
-              
-          }
-          
-            // console.log(response);
-            
-        } catch (error) {
-            console.log(error.response.data.error);
-        } 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const email = data.get("email");
+    const password = data.get("password");
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/login",
+        { email, password },
+        { withCredentials: true }
+      );
+      if (response.status == 200) {
+        setUser(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
